2bd000291c4d5db367f9b84a5a720f5649b60311
Precious Alikor <preciousalikor@gmail.com>
2023-08-09 23:46:44 +0100

    Update

--- components/Watch.vue
+++ components/Watch.vue
@@ -3,6 +3,11 @@
         <div class="container-lg mx-auto">
             <div class="grid lg:grid-cols-4 gap-6 my-16">
                 <div class="card pb-2">
+                    <img src="../assets/img/product-06.webp" alt="img">
+                    <p class="product-name mt-3 leading-6 text-sm">Vintage Inspired Classic</p>
+                    <p class="product-price mb-2 text-sm">$93.20</p>
+                </div>
+                <div class="card pb-2">
                     <img src="../assets/img/product-15.webp" alt="img">
                     <p class="product-name mt-3 leading-6 text-sm">Mini Silver Mesh Watch</p>
                     <p class="product-price mb-2 text-sm">$86.85</p>
